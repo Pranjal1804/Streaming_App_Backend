38c000d3373a404e49b006beaeebd2e9caad4eaf
Pranjal1804 <mitrapranjal2004@gmail.com>
2024-08-16 12:43:32 +0530

    added cors and cookie-parser dependencies and server built

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+const app=express();
+app.use(cors({
+    origin:process.env.CORS_URL,
+    credentials:true
+}))
+app.use(express.json({limit:"100kb"}))
+app.use(express.urlencoded({extended:true,limit:"50kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+export {app}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import connectDB  from "./db/index.js"
 import dotenv from "dotenv"
+import app from "./app.js"
 dotenv.config({
     path:'./env'
 })
 connectDB()
-
+.then(()=>{
+  app.listen(process.env.PORT||8000,()=>{
+    console.log(`Server is listening on port ${process.env.PORT}`);
+  })
+})
+.catch((error)=>{
+  console.log("MongoDB connection error");
+})
 
 
 
